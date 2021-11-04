86e03417c34e0fc26c0ad0087c2db3a8ab98cde4
HIMEHINA <1545340072@qq.com>
2021-11-05 04:26:23 +0800

    增加#With函数

--- src/Game/Systems/RenderingSystem.ts
+++ src/Game/Systems/RenderingSystem.ts
@@ -1,27 +1,59 @@
 import { System } from '../System';
-import '../Components/Shape';
-import { Shape } from '../Components/Shape';
+import '../Components/Renderable';
+import { Renderable } from '../Components/Renderable';
 import { Transform } from '../Components/Transform';
 import { Entity } from '../EntityManager';
 import { Renderer } from '../Renderer/Renderer';
 
-class RenderingSystem extends System {
-  renderer: Renderer;
+export class RenderingSystem extends System {
+  protected _offScreenCanvas: HTMLCanvasElement | null = null;
+  protected _offScreenRenderer: Renderer;
+  protected _renderer: Renderer;
 
   constructor(canvas: HTMLCanvasElement | null) {
     super();
-    this.renderer = new Renderer(canvas);
+    if (canvas) {
+      this._offScreenCanvas = canvas.cloneNode() as HTMLCanvasElement;
+      this._offScreenCanvas.id = "off-screen-canvas";
+    }
+    this._offScreenRenderer = new Renderer(this._offScreenCanvas);
+    this._renderer = new Renderer(canvas);
+  }
+
+  #With = (entity: Entity) => {
+    let transfrom = entity.GetComponent(Transform) as Transform;
+    let shape = entity.GetComponent(Renderable) as Renderable;
+    if (transfrom) {
+      this._offScreenRenderer.Translate(transfrom.glbPosition);
+      this._offScreenRenderer.Scale(transfrom.scale);
+      this._offScreenRenderer.Rotate(transfrom.rotation);
+      if (shape) {
+        this._offScreenRenderer.DrawPath(shape.path, shape.rendererSettings);
+        if (shape.img)
+          this._offScreenRenderer.DrawImage(
+            shape.img,
+            undefined,
+            shape.rendererSettings);
+      }
+    }
   }
 
   Update(shapeEntities: Entity[]) {
-    let i = 0;
-    while (i < shapeEntities.length) {
-      this.renderer;
-      ++i;
+    if (this._offScreenCanvas) {
+      this._offScreenRenderer.ClearCanvas();
+
+      let i = 0;
+      while (i < shapeEntities.length) {
+        this._offScreenRenderer.With(this.#With, shapeEntities[i]);
+        ++i;
+      }
+
+      this._renderer.ClearCanvas();
+      this._renderer.DrawImage(this._offScreenCanvas);
     }
   }
 
   Filter(entity: Entity) {
-    return entity.HasComponent(Transform) && entity.HasComponent(Shape);
+    return entity.HasComponent(Transform) && entity.HasComponent(Renderable);
   }
 }
