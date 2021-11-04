e45c1c481b6d10d24bee8484c6e2ff337d669563
HIMEHINA <1545340072@qq.com>
2021-11-05 04:29:12 +0800

    增加矩阵变换以及可变方法; 修改Unit函数

--- src/Game/utils/Vector.ts
+++ src/Game/utils/Vector.ts
@@ -2,57 +2,44 @@ import { Matrix } from './Matrix';
 import { ErrorHelper, IsType, IsTypePos, Pos } from './Utilities';
 
 export class Vector3 {
-  public readonly zero = new Vector3(0, 0, 0);
-  public readonly unitX = new Vector3(1, 0, 0);
-  public readonly unitY = new Vector3(0, 1, 0);
-  public readonly unitZ = new Vector3(0, 0, 1);
+  public static readonly zero = new Vector3(0, 0, 0);
+  public static readonly unitX = new Vector3(1, 0, 0);
+  public static readonly unitY = new Vector3(0, 1, 0);
+  public static readonly unitZ = new Vector3(0, 0, 1);
 
-  protected _x: number = 0;
-  protected _y: number = 0;
-  protected _z: number = 0;
+  public x: number = 0;
+  public y: number = 0;
+  public z: number = 0;
 
   constructor();
   constructor(x: number, y: number);
   constructor(x: number, y: number, z: number);
-  constructor(pos: Pos);
+  constructor(pos?: Pos);
   constructor(xOrPos?: number | Pos, y?: number, z?: number) {
-    if (IsTypePos(xOrPos) && IsType(y, 'undefined') && IsType(z, 'undefined')) {
-      this._x = xOrPos.x;
-      this._y = xOrPos.y;
-      this._z = xOrPos.z;
-    } else if (IsType(xOrPos, 'number') && IsType(y, 'number') && IsType(z, 'number')) {
-      this._x = xOrPos;
-      this._y = y;
-      this._z = z;
+    if (IsType(xOrPos, 'undefined') && IsType(y, 'undefined') && IsType(z, 'undefined')) {
+    } else if (IsTypePos(xOrPos) && IsType(y, 'undefined') && IsType(z, 'undefined')) {
+      this.x = xOrPos.x;
+      this.y = xOrPos.y;
+      this.z = xOrPos.z;
+    } else if (IsType(xOrPos, 'number') && IsType(y, 'number')) {
+      this.x = xOrPos;
+      this.y = y;
+      if (IsType(z, 'undefined')) {
+      } else if (IsType(z, 'number')) {
+        this.z = z;
+      } else {
+        return ErrorHelper.ErrConstructorArgs(Vector3);
+      }
     } else {
-      ErrorHelper.ErrConstructorArgs(Vector3);
+     return ErrorHelper.ErrConstructorArgs(Vector3);
     }
   }
 
 
-  get x() {
-    return this._x;
-  }
-  set x(newX: number) {
-    this._x = newX;
-  }
-  get y() {
-    return this._y;
-  }
-  set y(newY: number) {
-    this._y = newY;
-  }
-  get z() {
-    return this._z;
-  }
-  set z(newZ: number) {
-    this._z = newZ;
-  }
-
   set(v: Vector3): this {
-    this._x = v.x;
-    this._y = v.y;
-    this._z = v.z;
+    this.x = v.x;
+    this.y = v.y;
+    this.z = v.z;
     return this;
   }
 
@@ -64,40 +51,40 @@ export class Vector3 {
   }
   nomalize(): this {
     let norm = this.Norm();
-    this._x /= norm, this._y /= norm, this._z /= norm;
+    this.x /= norm, this.y /= norm, this.z /= norm;
     return this;
   }
   Id(this: Vector3) {
     return Vector3.Id(this);
   }
-  Equal(this: Vector3, other: Pos) {
-    return Vector3.Equal(this, other);
+  Equals(this: Vector3, other: Pos) {
+    return Vector3.Equals(this, other);
   }
   Opposite(this: Vector3) {
     return Vector3.Opposite(this);
   }
   opposite(): this {
-    this._x = -this._x;
-    this._y = -this._y;
-    this._z = -this._z;
+    this.x = -this.x;
+    this.y = -this.y;
+    this.z = -this.z;
     return this;
   }
   Add(this: Vector3, v: Pos) {
     return Vector3.Add(this, v);
   }
   add(v: Pos): this {
-    this._x += v.x;
-    this._y += v.y;
-    this._z += v.z;
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
     return this;
   }
   Sub(this: Vector3, v: Pos) {
     return Vector3.Sub(this, v);
   }
   sub(v: Pos): this {
-    this._x -= v.x;
-    this._y -= v.y;
-    this._z -= v.z;
+    this.x -= v.x;
+    this.y -= v.y;
+    this.z -= v.z;
     return this;
   }
   Cross(this: Vector3, v: Pos) {
@@ -110,55 +97,68 @@ export class Vector3 {
     return Vector3.Mul(this, k);
   }
   mul(k: number): this {
-    this._x *= k;
-    this._y *= k;
-    this._z *= k;
+    this.x *= k;
+    this.y *= k;
+    this.z *= k;
     return this;
   }
   Div(this: Vector3, k: number) {
     return Vector3.Div(this, k);
   }
   div(k: number): this {
-    this._x /= k;
-    this._y /= k;
-    this._z /= k;
+    this.x /= k;
+    this.y /= k;
+    this.z /= k;
     return this;
   }
   ApplyMatrix(mat: Matrix) {
     return Vector3.ApplyMatrix(mat, this);
   }
   applyMatrix(mat: Matrix): this {
-    const x = this._x, y = this._y, z = this._z;
-    this._x = mat.m(0, 0) * x + mat.m(0, 1) * y + mat.m(0, 2) * z;
-    this._y = mat.m(1, 0) * x + mat.m(1, 1) * y + mat.m(1, 2) * z;
-    this._z = mat.m(2, 0) * x + mat.m(2, 1) * y + mat.m(2, 2) * z;
+    const x = this.x, y = this.y, z = this.z;
+    this.x = mat.m(0, 0) * x + mat.m(0, 1) * y + mat.m(0, 2) * z;
+    this.y = mat.m(1, 0) * x + mat.m(1, 1) * y + mat.m(1, 2) * z;
+    this.z = mat.m(2, 0) * x + mat.m(2, 1) * y + mat.m(2, 2) * z;
     return this;
+  }
+
+  static RandUnit2D() {
+    return new Vector3(
+      Math.random() * 2 - 1,
+      Math.random() * 2 - 1,
+    ).nomalize();
   }
 
   static Norm(v: Pos) {
     return Math.sqrt(Vector3.Dot(v, v));
   }
-
   static Id(other: Pos | Vector3) {
     return new Vector3(other.x, other.y, other.z);
   }
-
-  static Unit(alpha: number, beta: number, gamma?: number) {
-    if (typeof gamma === 'number') {
+  static Unit(alpha: number, beta?: number, gamma?: number) {
+    if (IsType(gamma, 'number') && IsType(beta, 'number')) {
       return new Vector3(
         Math.cos(alpha),
         Math.cos(beta),
         Math.cos(gamma)
       );
-    } else {
+    } else if (IsType(beta, 'number')) {
       return new Vector3(
         Math.cos(beta) * Math.cos(alpha),
         Math.cos(beta) * Math.sin(alpha),
         Math.sin(beta)
       );
+    } else if (IsType(beta, 'undefined')) {
+      return new Vector3(
+        Math.cos(alpha),
+        Math.sin(alpha),
+      );
+    } else {
+      return ErrorHelper.ClassErrMsg(Vector3, Vector3.Unit, 'Wrong arguments!');
     }
   }
-  static Equal(v1: Pos, v2: Pos) {
+
+  static Equals(v1: Pos, v2: Pos) {
     return v1.x === v2.x && v1.y === v2.y && v1.z === v2.z;
   }
   static Opposite(v1: Pos) {
@@ -191,7 +191,7 @@ export class Vector3 {
   }
   static ApplyMatrix(mat: Matrix, v: Pos) {
     if (mat.col !== 3) {
-      ErrorHelper.ClassErrMsg(Vector3, Vector3.ApplyMatrix, 'Wrong matrix!');
+      return ErrorHelper.ClassErrMsg(Vector3, Vector3.ApplyMatrix, 'Wrong matrix!');
     }
     return new Vector3(
       mat.m(0, 0) * v.x + mat.m(0, 1) * v.y + mat.m(0, 2) * v.z,
