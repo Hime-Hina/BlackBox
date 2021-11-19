72d8ff07f5d084b117386236957aee73c1ddfe60
HIMEHINA <1545340072@qq.com>
2021-11-19 22:40:04 +0800

    添加游戏暂停功能(未测试)

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -1,6 +1,17 @@
 export abstract class Game {
+  protected _frameID: number = 0;
+  protected _hasStarted = false;
   constructor() {}
 
-  abstract Start(...args: any[]): void;
+  Start(...args: any[]) {
+    this._hasStarted = true;
+    console.log("Game start.");
+  }
   abstract Update: (timeStamp: number) => void;
+  Pause() {
+    if (this._hasStarted) {
+      this._hasStarted = false;
+      window.cancelAnimationFrame(this._frameID);
+    }
+  }
 }
